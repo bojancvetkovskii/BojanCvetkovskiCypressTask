f0601aa11a87a8ad81cde1e6afe93cab25dfa0e7
bojancvetkovskii <bojan.cvetkovski@testdevlab.com>
2022-11-11 15:29:00 +0100

    *Implemented new test case

--- cypress/e2e/authorization/registering-new-user.cy.js
+++ cypress/e2e/authorization/registering-new-user.cy.js
@@ -5,6 +5,10 @@ describe("Registering new user tests", () => {
         cy.get("#divSignIn").click()
         cy.url().should("include", "https://lv.sportsdirect.com/login")
     })
+    it("Successfully displayed the new customer section in log in page", () => {
+        cy.visit("/login")
+        cy.get(".newCustomer").get("div:first-of-type").get("h1").contains("New User?")
 
+    })
 
 })
