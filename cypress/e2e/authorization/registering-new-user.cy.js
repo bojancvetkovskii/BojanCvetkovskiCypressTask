6c1ae8feb02104a2eeef273d410bafc7d269325b
bojancvetkovskii <bojan.cvetkovski@testdevlab.com>
2022-11-11 15:30:53 +0100

    *Implemented new test case

--- cypress/e2e/authorization/registering-new-user.cy.js
+++ cypress/e2e/authorization/registering-new-user.cy.js
@@ -5,21 +5,27 @@ describe("Registering new user tests", () => {
         cy.get("#divSignIn").click()
         cy.url().should("include", "https://lv.sportsdirect.com/login")
     })
+
     it("Successfully displayed the new customer section in log in page", () => {
         cy.visit("/login")
         cy.get(".newCustomer").get("div:first-of-type").get("h1").contains("New User?")
 
     })
+
     it("Successfully redirected to the Sign up form from log in page", () => {
         cy.visit("/login")
         cy.get(".NewCustWrap:last-of-type").click()
         cy.url().should("include", "https://lv.sportsdirect.com/registration")
     })
+
     it("Successfully displayed the form for registering new user", () => {
         cy.visit("/registration")
         expect("#RegistrationForm").to.exist
     })
 
-
-
+    it("Should display errors when there are no data for the user", () => {
+        cy.visit("/registration")
+        RegisterForm.clickRegisterButton()
+        cy.url().should("include", "https://lv.sportsdirect.com/registration")
+    })
 })
