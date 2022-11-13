4ca92e26915cdbe5b814893d63985e290075a1d0
bojancvetkovskii <bojan.cvetkovski@testdevlab.com>
2022-11-13 18:04:02 +0100

    *Generated report

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
-const { defineConfig } = require('cypress')
+const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
+    projectId: "8a17qo",
     e2e: {
         chromeWebSecurity: false,
         baseUrl: "https://lv.sportsdirect.com",
+        watchForFileChanges: false,
+        reporter: "mochawesome",
+        reporterOptions: {
+            reportDir: "cypress/results",
+            html: true,
+            overwrite: true,
+            reportFilename: "report"
+        },
         setupNodeEvents(on, config) {
         },
-    }
+    },
 })
